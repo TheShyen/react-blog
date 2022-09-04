5b87cc4a83fd1de65f0d346c720c1201c63768b1
TheShyen <ruslankaslesh@mail.ru>
2022-09-04 23:59:49 +0500

    preparation and start

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,10 @@
+import './Footer.css';
+
+const Footer = () => {
+    return (
+        <footer>
+            <span>© TheShy React Blog</span>
+        </footer>
+    );
+};
+export default Footer;
---
+++ src/components/Header/Header.js
@@ -0,0 +1,14 @@
+import "./Header.css";
+
+const Header = () => {
+    return (
+        <header>
+            <nav>
+                <a href="#first">Aboba</a>
+                <a href="#second">Aboba</a>
+                <a href="#third">Aboba</a>
+            </nav>
+        </header>
+    );
+};
+export default Header;
